76a334a5e5b80b24d334da1da932792dba0cf911
YanjinlkhamM <bbyykkdd1999@gmail.com>
2025-10-14 21:15:10 +0800

    header design fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,21 +28,30 @@ export default function Home() {
           <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
         <Header />
-        <main className="relative z-10 min-h-screen flex flex-col items-center justify-center text-white text-center px-4">
-         <div className="absolute flex flex-col items-start" style={{bottom: "80px" , left:"65px"}}>
-           <h1 className="text-5xl md:text-6xl font-bold tracking-wider">
-            {t("main_title")}
-          </h1>
-          <p className="text-xl md:text-2xl mt-4 mb-6 font-light">
-            {t("subtitle")}
-          </p>
-          <p className="max-w-2xl mx-auto text-sm md:text-base leading-relaxed mb-8 whitespace-pre-line text-start">
-            {t("description")}
-          </p>
-          <button className="bg-transparent border border-white rounded-full text-white font-bold py-3 px-8 text-sm hover:bg-white hover:text-black transition-colors duration-300"  onClick={() => (window.location.href = "/product")}>
-            {t("explore_button")}
-          </button>
-         </div>
+        <main className="relative z-10 min-h-screen flex flex-col items-center justify-center text-white text-center">
+          <div
+            className="absolute flex flex-col items-start"
+            style={{ bottom: "80px", left: "76px" }}
+          >
+            <h1
+              className="font-bold tracking-wider"
+              style={{ fontSize: "60px", marginBottom: "8px" }}
+            >
+              {t("main_title")}
+            </h1>
+            <p className="text-xl md:text-2xl mt-4 mb-6 font-light">
+              {t("subtitle")}
+            </p>
+            <p className="max-w-2xl mx-auto text-sm md:text-base leading-relaxed mb-8 whitespace-pre-line text-start">
+              {t("description")}
+            </p>
+            <button
+              className="bg-transparent border border-white rounded-full text-white font-bold py-3 px-8 text-sm hover:bg-white hover:text-black transition-colors duration-300"
+              onClick={() => (window.location.href = "/product")}
+            >
+              {t("explore_button")}
+            </button>
+          </div>
         </main>
       </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,42 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import MenuModal from './MenuModal';
-import '../i18n';
-import Link from 'next/link';
-import Image from 'next/image';
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import MenuModal from "./MenuModal";
+import "../i18n";
+import Link from "next/link";
+import Image from "next/image";
 
-const MenuIcon = ({ color }: { color: string }) => (
-  <svg className={`w-6 h-6 ${color}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-  </svg>
+const MenuIcon = ({
+  colorConv,
+  isSolid,
+}: {
+  colorConv: string;
+  isSolid: boolean;
+}) => (
+  <Image
+    color={colorConv}
+    className={`w-6 h-6 align-middle ${isSolid ? "filter invert" : ""}`}
+    src="/icons_menu_black.svg"
+    alt="menu"
+    width={36}
+    height={37.67}
+  />
 );
 
 interface HeaderProps {
-  variant?: 'transparent' | 'solid';
+  variant?: "transparent" | "solid";
 }
 
-const Header: React.FC<HeaderProps> = ({ variant = 'transparent' }) => {
+const Header: React.FC<HeaderProps> = ({ variant = "transparent" }) => {
   const { i18n } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLangMenuOpen, setIsLangMenuOpen] = useState(false);
 
-  const isSolid = variant === 'solid';
-  const textColor = isSolid ? 'text-black' : 'text-white';
-  const bgColor = isSolid ? 'bg-white shadow-md' : 'bg-transparent';
-  const logoColor = isSolid ? 'text-gray-800' : 'text-white';
+  const isSolid = variant === "solid";
+  const textColor = isSolid ? "text-black" : "text-white";
+  const bgColor = isSolid ? "bg-white" : "bg-transparent";
+  const logoColor = isSolid ? "text-gray-800" : "text-white";
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -38,26 +49,65 @@ const Header: React.FC<HeaderProps> = ({ variant = 'transparent' }) => {
 
   return (
     <>
-      <header className={`absolute top-0 left-0 w-full z-30 ${bgColor} ${textColor}`}>
-        <div className="w-full mx-auto px-6 sm:px-10 py-4 flex justify-between items-center">
+      <header
+        className={`fixed top-[50px] left-[76px] right-[76px] z-30 ${bgColor} ${textColor}`}
+      >
+        <div className="flex justify-between items-start ">
           <Link href="/" className={`flex flex-col items-start ${logoColor}`}>
-            <span className="text-xs font-light">AI CADDIE</span>
-            <span className="text-2xl font-bold tracking-wider">FIELDBOOK</span>
+            <Image
+              src="/BI.svg"
+              alt="logo"
+              className={`align-middle ${isSolid ? "filter invert" : ""}`}
+              width={130}
+              height={86}
+            />{" "}
           </Link>
-          <div className="flex items-center space-x-5">
-            <div className="relative">
-              {/* <button onClick={() => setIsLangMenuOpen(!isLangMenuOpen)} className="focus:outline-none">
-                  <Image src="/language.svg" alt="Language" className={`w-6 h-6 ${isSolid ? 'filter invert' : ''}`} />
-                </button> */}
+          <div className="flex items-center space-x-5 h-full">
+            <div className="relative flex items-center">
+              <button
+                onClick={() => setIsLangMenuOpen(!isLangMenuOpen)}
+                className="flex items-center justify-center focus:outline-none"
+              >
+                <Image
+                  src="/language.svg"
+                  alt="Language"
+                  className={`w-6 h-6 align-middle ${
+                    isSolid ? "filter invert" : ""
+                  }`}
+                  width={38}
+                  height={38}
+                />
+              </button>
               {isLangMenuOpen && (
                 <div className="absolute top-full right-0 mt-2 bg-white border border-gray-200 rounded-lg p-1 shadow-lg">
-                  <button onClick={() => changeLanguage('ko')} className={`block w-full text-left px-3 py-1 text-sm text-black ${i18n.language === 'ko' ? 'font-bold' : ''}`}>KOR</button>
-                  <button onClick={() => changeLanguage('en')} className={`block w-full text-left px-3 py-1 text-sm text-black ${i18n.language === 'en' ? 'font-bold' : ''}`}>ENG</button>
+                  <button
+                    onClick={() => changeLanguage("ko")}
+                    className={`block w-full text-left px-3 py-1 text-sm text-black ${
+                      i18n.language === "ko" ? "font-bold" : ""
+                    }`}
+                  >
+                    KOR
+                  </button>
+                  <button
+                    onClick={() => changeLanguage("en")}
+                    className={`block w-full text-left px-3 py-1 text-sm text-black ${
+                      i18n.language === "en" ? "font-bold" : ""
+                    }`}
+                  >
+                    ENG
+                  </button>
                 </div>
               )}
             </div>
-            <button onClick={toggleMenu} className="focus:outline-none">
-              <MenuIcon color={isSolid ? 'text-black' : 'text-white'} />
+
+            <button
+              onClick={toggleMenu}
+              className="flex items-center justify-center focus:outline-none"
+            >
+              <MenuIcon
+                colorConv={isSolid ? "text-black" : "text-white"}
+                isSolid={isSolid}
+              />
             </button>
           </div>
         </div>
